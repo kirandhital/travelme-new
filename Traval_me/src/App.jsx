c12f2f513a7c6b2d5e17196a9025901739e59fb4
kirandhital <dhitalkira@gmail.com>
2025-09-23 08:05:56 +0545

    fix

--- Traval_me/src/App.jsx
+++ Traval_me/src/App.jsx
@@ -2,22 +2,15 @@ import React from 'react'
 import Home from './Pages/Home'
 import About from './Pages/About'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route  
-        path='/'
-        element={<Home />} >
-        </Route>
-         <Route  
-        path='/about'
-        element={<About />} >
-        </Route>
-
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
       </Routes>
-    </BrowserRouter>
-
+    </Router>
   )
 }
 
